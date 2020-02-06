e389a4dd782dca08e3f3573d68b80ae876212da7
Leviii99 <nyiro.levente@gmail.com>
2020-02-07 00:00:34 +0100

    Internetkapcsolat ellenőrzése

--- app/src/main/java/com/leventenyiro/firebaseprototype/LoginActivity.java
+++ app/src/main/java/com/leventenyiro/firebaseprototype/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,26 +44,30 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (inputUsernameEmail.getText().toString().isEmpty() || inputPassword.getText().toString().isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Valami nincs kitöltve!", Toast.LENGTH_SHORT).show();
-                }
+                if (!isNetworkConnected())
+                    Toast.makeText(LoginActivity.this, "Nincs internetkapcsolat!", Toast.LENGTH_SHORT).show();
                 else {
-                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("user");
-                    ref.orderByChild("username").equalTo(inputUsernameEmail.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            if (dataSnapshot.exists()) {
-                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                                    if (dataSnapshot.getChildrenCount() == 1)
-                                        login(String.valueOf(snapshot.child("email").getValue()));
+                    if (inputUsernameEmail.getText().toString().isEmpty() || inputPassword.getText().toString().isEmpty()) {
+                        Toast.makeText(LoginActivity.this, "Valami nincs kitöltve!", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("user");
+                        ref.orderByChild("username").equalTo(inputUsernameEmail.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.exists()) {
+                                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                        if (dataSnapshot.getChildrenCount() == 1)
+                                            login(String.valueOf(snapshot.child("email").getValue()));
+                                    }
                                 }
+                                else
+                                    login(inputUsernameEmail.getText().toString());
                             }
-                            else
-                                login(inputUsernameEmail.getText().toString());
-                        }
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) { }
-                    });
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) { }
+                        });
+                    }
                 }
             }
         });
@@ -70,8 +75,12 @@ public class LoginActivity extends AppCompatActivity {
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegActivity.class));
-                finish();
+                if (!isNetworkConnected())
+                    Toast.makeText(LoginActivity.this, "Nincs internetkapcsolat!", Toast.LENGTH_SHORT).show();
+                else {
+                    startActivity(new Intent(LoginActivity.this, RegActivity.class));
+                    finish();
+                }
             }
         });
     }
@@ -108,5 +117,10 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 }
